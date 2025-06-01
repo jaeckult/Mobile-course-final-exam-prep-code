10cb90d36e7f598631cf1fd0a96f96cfa0d4b0eb
YITBAREK ALEMU <160623517+jaeckult@users.noreply.github.com>
2025-06-01 20:23:18 +0300

    List view

---
+++ flutter-intro/counterapp/lib/listView.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Scaffold Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 76, 89, 175),
+        ),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text(title)), body: _buildListView());
+  }
+
+  Widget _buildListView() => ListView.builder(
+    itemCount: 100,
+    itemBuilder: (context, index) {
+      return Text('Item $index');
+    },
+  );
+}
